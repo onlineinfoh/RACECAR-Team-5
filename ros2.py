2caa58caeb7efd40a7d7a6dd32e3a1668503e3d7
Onlineinfoh <tianxi730@gmail.com>
2025-07-19 22:56:12 -0400

    added three versiops of accel calculation, with kalman filter

--- ros2.py
+++ ros2.py
@@ -37,6 +37,26 @@ def clamp_angle(angle):
             angle -= 2*np.pi
     return angle
 
+# at the top of your file, _above_ CompFilterNode:
+
+class OneDKalman:
+    def __init__(self, process_var=0.01, sensor_var=1.0):
+        # Q = process noise variance, R = measurement noise variance
+        self.Q = process_var
+        self.R = sensor_var
+        # state estimate and covariance
+        self.x = 0.0
+        self.P = 1.0
+
+    def update(self, z: float) -> float:
+        # 1) compute Kalman gain
+        K = self.P / (self.P + self.R)
+        # 2) update estimate
+        self.x = self.x + K * (z - self.x)
+        # 3) update covariance
+        self.P = (1 - K) * self.P + self.Q
+        return self.x
+
 class CompFilterNode(Node):
     def __init__(self):
         super().__init__('complementary_filter_node')
@@ -76,8 +96,13 @@ class CompFilterNode(Node):
         self.x_pos = 0.0
         self.y_pos = 0.0
 
+        ### Kalman Filter
+        self.kf_ax = OneDKalman(process_var=0.01, sensor_var=0.5)
+        self.kf_ay = OneDKalman(process_var=0.01, sensor_var=0.5)
+        self.kf_az = OneDKalman(process_var=0.01, sensor_var=0.5)
 
-    # Called when new IMU data is received, attitude calc completed here as well
+
+    # Called when new IMU data is received, attitude calculation completed here as well
     def imu_callback(self, data):
 
         # Grab linear acceleration and angular velocity values from subscribed data points
@@ -91,10 +116,8 @@ class CompFilterNode(Node):
 
         # Calculate time delta
         now = [data.header.stamp.sec, data.header.stamp.nanosec] # Current ROS time
-        dt = now[0] + 1e-9 * now[1] - self.prev_time  # Time delta
-        print(f"dt: {dt} seconds")
+        dt = now[0] + 1e-9 * now[1] - self.prev_time  # Time 
         self.prev_time = now[0] + 1e-9 * now[1] # refresh checkpoint
-
 
         # tilt angle calculation using linear acceleration data
         tilt_angle = [np.arctan2(accel[1], accel[2]), np.arctan2(-accel[0], np.sqrt(accel[1] * accel[1] + accel[2] * accel[2])), 0]
@@ -122,32 +145,80 @@ class CompFilterNode(Node):
             # since the tilt angle from the accelerometer is 0 
             # (since the gravity vector is not changed when rotating around the z axis)
             # the tilt needs to be compensated using the magnetometer values
+
             tilt_compensated_mag = [mx * np.cos(x) + my * np.sin(y) + mz * np.sin(x) * np.cos(y), 
                                     my * np.cos(y)-mz*np.sin(y),
                                     -mx * np.sin(x) + my * np.cos(x) * np.sin(y) + mz * np.cos(x) * np.cos(y)]
+            
             mag_accel_yaw=np.arctan2(-tilt_compensated_mag[1],tilt_compensated_mag[0])
 
-            tilt_b=np.array([np.cos(x),np.sin(x)*np.sin(y),np.sin(x)*np.cos(y)],[0,np.cos(y),-np.sin(y)],[-np.sin(x),np.cos(x)*np.sin(y),np.cos(x)*np.sin(y)])
-            tilt_b_inv=np.linalg.inv(tilt_b)
+
+            # velocity calculation
+
+            ################################################################
+            # 1. With matrix compensation, tilt -> compensate for g, -> untilt
+            # tilt to rotate the acceleration matrix
+            tilt_b=np.array([
+                [np.cos(x),np.sin(x)*np.sin(y),np.sin(x)*np.cos(y)],
+                [0,np.cos(y),-np.sin(y)],
+                [-np.sin(x),np.cos(x)*np.sin(y),np.cos(x)*np.sin(y)]
+            ])
+
+            # tilt the acceleration matrix to match the global axis
             accel_matrix=np.array(accel)
-            tilt_compensated_accel=np.matmul(tilt_b_inv, accel_matrix)
+            tilt_compensated_accel=np.matmul(tilt_b, accel_matrix)
             tilt_compensated_accel[2]+=9.81
-            print(tilt_compensated_accel)
+
+            ################################################################
+
+            ################################################################
+            # 2. Do not anything
+            tilt_compensated_accel = accel
+            ################################################################
+
+            ################################################################
+            # 3. Direct compensation
+            g = 9.81
+            tilt_compensated_accel = [
+                accel[0] - g * np.sin(self.gyro_pitch),
+                accel[1] - g * np.sin(self.gyro_roll),
+                accel[2] - g * np.cos(self.gyro_pitch) * np.cos(self.gyro_roll)
+            ]
+            ################################################################
+            
+            # Kalman filter
+            ax_f = self.kf_ax.update(float(tilt_compensated_accel[0]))
+            ay_f = self.kf_ax.update(float(tilt_compensated_accel[1]))
+            az_f = self.kf_ax.update(float(tilt_compensated_accel[2]))
+
+            # Normal calculation
+            self.v_x += dt * tilt_compensated_accel[0]
+            self.v_y += dt * tilt_compensated_accel[1]
+            self.v_z += dt * tilt_compensated_accel[2]
+
+            # Kalman filter calculation
+            self.v_x += dt * ax_f
+            self.v_y += dt * ay_f
+            self.v_z += dt * az_f
+            
+            print(f"raw accel: {tilt_compensated_accel}, filt accel: {[ax_f, ay_f, az_f]}")
+
+            print(self.v_x, self.v_y. self.v_z)
         else:
             mag_accel_yaw = self.yaw
         
         # TODO: Fuse gyro, mag, and accel derivations in complementary filter
         self.roll  = self.alpha * self.gyro_roll + (1.0-self.alpha) * x
         self.pitch = self.alpha * self.gyro_pitch + (1.0-self.alpha) * y
-        self.yaw = self.alpha * self.gyro_yaw + (1.0-self.alpha) * mag_theta_z
+        self.yaw = self.alpha * self.gyro_yaw + (1.0-self.alpha) * mag_accel_yaw
 
         # Print results for sanity checking
 
         print(f"====== Complementary Filter Results ======")
         print(f"Speed || Freq = {round(1/dt,0)} || dt (ms) = {round(dt*1e3, 2)}")
         print(f"Accel + Mag Derivation")
-        print(f"Roll (deg): {accel_roll * 180/math.pi}")
-        print(f"Pitch (deg): {accel_pitch * 180/math.pi}")
+        print(f"Roll (deg): {x * 180/math.pi}")
+        print(f"Pitch (deg): {y * 180/math.pi}")
         print(f"Yaw (deg): {mag_accel_yaw * 180/math.pi}")
         print()
         print(f"Gyro Derivation")
