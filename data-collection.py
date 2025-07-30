f0042a660470f21f389eb1e97f674722b0cc2573
Krista Sebastian <krista@Binos-MacBook-Air.local>
2025-07-30 15:30:59 -0400

    cleanup and comments for data collection

--- data-collection.py
+++ data-collection.py
@@ -18,29 +18,32 @@ contour_area = 0
 # Called once on start
 def start():
     rc.drive.set_speed_angle(0, 0)
-    rc.set_update_slow_time(0.5)
+    rc.set_update_slow_time(0.5) # sets update slow frequency to 2 Hz
 
 # Called every frame
 def update():
-    # Manual override for testing
-    speed = 0
-    if rc.controller.get_trigger(rc.controller.Trigger.RIGHT) > 0:
-        speed = 1
-    elif rc.controller.get_trigger(rc.controller.Trigger.LEFT) > 0:
-        speed = -1
 
-    x, _ = rc.controller.get_joystick(rc.controller.Joystick.LEFT)
-    angle = 1 if x > 0 else -1 if x < 0 else 0
 
+    # MANUAL DRIVE
+    # speed determined by left joystick
+    x, y = rc.controller.get_joystick(rc.controller.Joystick.LEFT)
+    speed=x
+
+    # angle determined by right joystick
+    a,b=rc.controller.get_joystick(rc.controller.Joystick.RIGHT)
+    angle=a
+
+    # sets speed and angle to determined values
     rc.drive.set_speed_angle(speed, angle)
 
+
     # Capture and save an image when A button is pressed
-    if rc.controller.was_pressed(rc.controller.Button.A):
-        os.makedirs('data', exist_ok=True)
-        img = rc.camera.get_color_image()
-        filename = f"data/image_{int(time.time())}.jpeg"
-        cv.imwrite(filename, img)
-        print(f"Saved image to {filename}")
+    if rc.controller.was_pressed(rc.controller.Button.A): # runs if A button is pressed
+        os.makedirs('data', exist_ok=True) # creates data directory if it doesn't exist already
+        img = rc.camera.get_color_image() # gets image from camera
+        filename = f"data/image_{int(time.time())}.jpeg" # makes a unique filename using timestamp
+        cv.imwrite(filename, img) # saves image to file
+        print(f"Saved image to {filename}") # confirms save
 
 # Called once per second
 def update_slow():
